fa39d051ab1ef0e8cd30df27d4914fe25e7498a3
shoheiyamamoto <nitiham55@yahoo.co.jp>
2018-07-16 23:12:11 +0900

    ScriptTest課題提出用
    1回目

---
+++ Assets/Test.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        // 要素数5の配列を初期化する
+        int[] array= new int[5];
+
+        // 配列の各要素に値を代入する
+        array[0] = 30;
+        array[1] = 20;
+        array[2] = 50;
+        array[3] = 10;
+        array[4] = 80;
+
+        // 配列の要素をすべて表示する
+        for (int i = 4; i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+        }
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
